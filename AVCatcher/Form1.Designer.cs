be93af7e806f583a712eacd0a0c86f20ebaa0df6
Jean Leal <Jean Leal>
2016-01-05 20:56:30 -0300

    projeto v1

---
+++ AVCatcher/Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace AVCatcher
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.baixarBtn = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.arquivoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sobreToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sairToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.videoRadio = new System.Windows.Forms.RadioButton();
+            this.audioRadio = new System.Windows.Forms.RadioButton();
+            this.listVideos = new System.Windows.Forms.ListBox();
+            this.rateLabel = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // baixarBtn
+            // 
+            this.baixarBtn.Location = new System.Drawing.Point(415, 53);
+            this.baixarBtn.Name = "baixarBtn";
+            this.baixarBtn.Size = new System.Drawing.Size(75, 23);
+            this.baixarBtn.TabIndex = 0;
+            this.baixarBtn.Text = "Baixar";
+            this.baixarBtn.UseVisualStyleBackColor = true;
+            this.baixarBtn.Click += new System.EventHandler(this.btnDownload_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(178, 27);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(312, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(178, 298);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(312, 15);
+            this.progressBar.TabIndex = 2;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.arquivoToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(502, 24);
+            this.menuStrip1.TabIndex = 4;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // arquivoToolStripMenuItem
+            // 
+            this.arquivoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.sobreToolStripMenuItem,
+            this.sairToolStripMenuItem});
+            this.arquivoToolStripMenuItem.Name = "arquivoToolStripMenuItem";
+            this.arquivoToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
+            this.arquivoToolStripMenuItem.Text = "Arquivo";
+            // 
+            // sobreToolStripMenuItem
+            // 
+            this.sobreToolStripMenuItem.Name = "sobreToolStripMenuItem";
+            this.sobreToolStripMenuItem.Size = new System.Drawing.Size(104, 22);
+            this.sobreToolStripMenuItem.Text = "Sobre";
+            // 
+            // sairToolStripMenuItem
+            // 
+            this.sairToolStripMenuItem.Name = "sairToolStripMenuItem";
+            this.sairToolStripMenuItem.Size = new System.Drawing.Size(104, 22);
+            this.sairToolStripMenuItem.Text = "Sair";
+            // 
+            // videoRadio
+            // 
+            this.videoRadio.AutoSize = true;
+            this.videoRadio.Checked = true;
+            this.videoRadio.Location = new System.Drawing.Point(178, 56);
+            this.videoRadio.Name = "videoRadio";
+            this.videoRadio.Size = new System.Drawing.Size(54, 17);
+            this.videoRadio.TabIndex = 5;
+            this.videoRadio.TabStop = true;
+            this.videoRadio.Text = "Vídeo";
+            this.videoRadio.UseVisualStyleBackColor = true;
+            // 
+            // audioRadio
+            // 
+            this.audioRadio.AutoSize = true;
+            this.audioRadio.Location = new System.Drawing.Point(178, 79);
+            this.audioRadio.Name = "audioRadio";
+            this.audioRadio.Size = new System.Drawing.Size(52, 17);
+            this.audioRadio.TabIndex = 6;
+            this.audioRadio.Text = "Audio";
+            this.audioRadio.UseVisualStyleBackColor = true;
+            // 
+            // listVideos
+            // 
+            this.listVideos.FormattingEnabled = true;
+            this.listVideos.Location = new System.Drawing.Point(0, 27);
+            this.listVideos.Name = "listVideos";
+            this.listVideos.Size = new System.Drawing.Size(172, 290);
+            this.listVideos.TabIndex = 7;
+            // 
+            // rateLabel
+            // 
+            this.rateLabel.AutoSize = true;
+            this.rateLabel.Location = new System.Drawing.Point(239, 53);
+            this.rateLabel.Name = "rateLabel";
+            this.rateLabel.Size = new System.Drawing.Size(0, 13);
+            this.rateLabel.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(502, 325);
+            this.Controls.Add(this.rateLabel);
+            this.Controls.Add(this.listVideos);
+            this.Controls.Add(this.audioRadio);
+            this.Controls.Add(this.videoRadio);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.baixarBtn);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button baixarBtn;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem arquivoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sobreToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sairToolStripMenuItem;
+        private System.Windows.Forms.RadioButton videoRadio;
+        private System.Windows.Forms.RadioButton audioRadio;
+        private System.Windows.Forms.ListBox listVideos;
+        private System.Windows.Forms.Label rateLabel;
+    }
+}
+
---
+++ AVCatcher/Form1.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using YoutubeExtractor;
+
+namespace AVCatcher
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnDownload_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text.Length > 0)
+            {
+                if (videoRadio.Checked)
+                {
+                    new Thread(DownloaderVideo).Start();
+                }
+                else if (audioRadio.Checked)
+                {
+                    new Thread(DownloaderAudio).Start();
+                }
+                
+            }
+            else
+            {
+                MessageBox.Show("Texto Vazio");
+            }
+        }
+
+        private void DownloaderVideo()
+        {
+            try
+            {
+                addItemToList("Iniciando Download...");
+                string link = textBox1.Text;
+                IEnumerable<VideoInfo> videoInfos = DownloadUrlResolver.GetDownloadUrls(link);
+                VideoInfo video = videoInfos.First(info => info.VideoType == VideoType.Mp4 && info.Resolution == 360);
+                if (video.RequiresDecryption)
+                {
+                    DownloadUrlResolver.DecryptDownloadUrl(video);
+                }
+                addItemToList("Baixando " + video.Title);
+                string outputTitle = video.Title;
+                foreach (char c in System.IO.Path.GetInvalidFileNameChars())
+                {
+                    outputTitle = outputTitle.Replace(c, '_');
+                }
+                var videoDownloader = new VideoDownloader(video, Path.Combine("C:/Downloads", outputTitle + video.VideoExtension));
+                videoDownloader.DownloadProgressChanged += (sender2, args) => this.Invoke((MethodInvoker)delegate {
+                    progressBar.Value = (int)(Math.Round(args.ProgressPercentage, 2));
+                    rateLabel.Text = Math.Round(args.ProgressPercentage, 2) + "%";
+                });
+                videoDownloader.DownloadFinished += (sender2, args) => this.Invoke((MethodInvoker)delegate {
+                    progressBar.Value = 0;
+                    listVideos.Items.Add("Download Finalizado!");
+                    rateLabel.Text = "";
+                });
+                videoDownloader.Execute();
+            }
+            catch(Exception e)
+            {
+                this.Invoke((MethodInvoker)delegate
+                {
+                    MessageBox.Show("Falha ao Baixar o vídeo " + e.Message);
+                });
+            }
+        }
+
+        private void addItemToList(string text)
+        {
+            this.Invoke((MethodInvoker)delegate {
+                listVideos.Items.Add(text);
+            });
+        }
+        private void DownloaderAudio()
+        {
+            try
+            {
+                string link = textBox1.Text;
+                addItemToList("Iniciando Download...");
+                IEnumerable<VideoInfo> videoInfos = DownloadUrlResolver.GetDownloadUrls(link);
+                VideoInfo video = videoInfos
+                    .Where(info => info.CanExtractAudio)
+                    .OrderByDescending(info => info.AudioBitrate)
+                    .First();
+
+                if (video.RequiresDecryption)
+                {
+                    DownloadUrlResolver.DecryptDownloadUrl(video);
+                }
+                addItemToList("Baixando " + video.Title);
+                string outputTitle = video.Title;
+                foreach (char c in System.IO.Path.GetInvalidFileNameChars())
+                {
+                    outputTitle = outputTitle.Replace(c, '_');
+                }
+                var audioDownloader = new AudioDownloader(video, Path.Combine("C:/Downloads", outputTitle + video.AudioExtension));
+                audioDownloader.DownloadProgressChanged += (sender, args) => this.Invoke((MethodInvoker)delegate {
+                    progressBar.Value = (int)(args.ProgressPercentage * 0.85);
+                    rateLabel.Text = Math.Round(args.ProgressPercentage * 0.85, 2).ToString() + "%";
+                });
+                audioDownloader.AudioExtractionProgressChanged += (sender, args) => this.Invoke((MethodInvoker)delegate {
+                    progressBar.Value = (int)(85 + args.ProgressPercentage * 0.15);
+                    rateLabel.Text = Math.Round(85 + args.ProgressPercentage * 0.15, 2).ToString() + "%";
+                });
+                audioDownloader.DownloadFinished += (sender, args) => this.Invoke((MethodInvoker)delegate {
+                    progressBar.Value = 0;
+                    listVideos.Items.Add("Download Finalizado!");
+                    rateLabel.Text = "";
+                });
+                
+                audioDownloader.Execute();
+            }
+            catch(Exception e)
+            {
+                this.Invoke((MethodInvoker)delegate
+                {
+                    MessageBox.Show("Falha ao Baixar o audio " + e.Message);
+                });
+            }
+        }
+    }
+}
